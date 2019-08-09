316ba37acf8d94c448f50e52a88821570abe499e
Fubuki (ADMIN) <Fubuki (ADMIN)@DESKTOP-T3LMQRU>
2019-08-09 11:03:42 +0530

    This is one of the first programs on my GitHub page.

---
+++ src/gitTest/TestPush.java
@@ -0,0 +1,10 @@
+package gitTest;
+
+public class TestPush {
+
+	public static void main(String[] args) {
+		// This is the first program of the test project to be pushed on GitHub
+		System.out.println("Hello World !!");
+	}
+
+}
