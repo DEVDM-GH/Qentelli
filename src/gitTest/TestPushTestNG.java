a50f69d2e77584a37abdbe33daf02f987549e427
Fubuki (ADMIN) <Fubuki (ADMIN)@DESKTOP-T3LMQRU>
2019-08-09 11:20:58 +0530

    1st Commit of TestNG prog.

---
+++ src/gitTest/TestPushTestNG.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestPushTestNG {
+	@Test
+	public void test() {
+		Reporter.log("Wow this is happening.. It's Good !!!",true);
+	}
+}
